10e9b0aa0f30723ec5640abd65d77f4cf358912b
Maria Thomas and Paul Guevarra <paul@>
2017-07-05 11:52:13 -0700

    add dog2.jpeg and cat2.jpeg and both button

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,9 @@ $(document).ready(function() {
     $("ul#dogs").prepend("<li>Aw Aw!</li>");
     $("ul#cats").prepend("<li>Meow!</li>");
   });
+
+  $("button#both").click(function() {
+    $("ul#dogs").prepend("<li><img width='100%' src='img/dog2.jpeg'></li>");
+    $("ul#cats").prepend("<li><img width='100%' src='img/cat2.jpeg'></li>");
+  });
 });
